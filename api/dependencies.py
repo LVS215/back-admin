e7494634e54a9796b374113a5a4dfcd103c1a910
LVS215 <idkjams5@gmail.com>
2026-01-18 22:08:41 +0300

    Update dependencies.py

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,1 +1,20 @@
+from ninja import Query
+from typing import Optional
 
+class PaginationParams(Schema):
+    page: int = 1
+    page_size: int = 10
+    
+    @property
+    def offset(self):
+        return (self.page - 1) * self.page_size
+    
+    @property
+    def limit(self):
+        return self.page_size
+
+class FilterParams(Schema):
+    category: Optional[int] = None
+    author: Optional[int] = None
+    status: Optional[str] = None
+    search: Optional[str] = None
