d9cae3b3956dd88d049104e1ee64b518e589e6e4
LVS215 <idkjams5@gmail.com>
2026-01-18 22:33:21 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from api.router import api
+from api.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
